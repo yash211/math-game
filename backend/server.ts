c02468de87aca951797eee64e4b189ec08733e34
Yash Gupta <gupta.yash211@gmail.com>
2025-03-12 02:31:53 -0400

    backend-url-bugfix

--- backend/server.ts
+++ backend/server.ts
@@ -11,7 +11,7 @@ const app = express();
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     methods: ["GET", "POST"]
   }
 });
